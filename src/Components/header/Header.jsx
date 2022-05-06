9b11941d75cf31a1699513c7b2af2ec634e3088f
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-05-06 15:04:42 +0530

    first commit

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,48 @@
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className=".headerTitle">A Lifetime of discounts? It's Genius</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free NV Booking account
+        </p>
+        <button className="headerBtn">Sign In/ Register</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Components/header/Header";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
